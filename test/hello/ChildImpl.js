2a4a11cb5394eb84a52935f3146488eb06833296
Antonio Lain <antlai@cafjs.com>
2014-10-19 13:57:23 -0700

    Test lease ownership

--- test/hello/ChildImpl.js
+++ test/hello/ChildImpl.js
@@ -35,26 +35,52 @@ exports.newInstance = function($, spec, cb) {
             return uuid;
         };
 
-        that.grabLease = function(cb0) {
-            $._.$.cp.grabLease(uuid, spec.env.leaseTimeout, cb0);
+        that.grabLease = function(cp, cb0) {
+            if ((typeof cp === 'function') && (cb0 === undefined)) {
+                cb0 = cp;
+                cp = 'cp';
+            }
+            $._.$[cp].grabLease(uuid, spec.env.leaseTimeout, cb0);
         };
 
-        that.renewLease = function(cb0) {
-            $._.$.cp.renewLeases([uuid], spec.env.leaseTimeout, cb0);
+        that.renewLease = function(cp, cb0) {
+            if ((typeof cp === 'function') && (cb0 === undefined)) {
+                cb0 = cp;
+                cp = 'cp';
+            }
+            $._.$[cp].renewLeases([uuid], spec.env.leaseTimeout, cb0);
         };
 
-        that.updateState = function(cb0) {
+        that.updateState = function(cp, cb0) {
+            if ((typeof cp === 'function') && (cb0 === undefined)) {
+                cb0 = cp;
+                cp = 'cp';
+            }
             counter = counter + 1;
-            $._.$.cp.updateState(uuid, JSON.stringify(that.readLocalState()),
-                                 cb0);
+            var cb1 = function(err, data) {
+                if (err) {
+                    counter = counter - 1;
+                }
+                cb0(err, data);
+            };
+            $._.$[cp].updateState(uuid, JSON.stringify(that.readLocalState()),
+                                  cb1);
         };
 
-        that.deleteState = function(cb0) {
-            $._.$.cp.deleteState(uuid, cb0);
+        that.deleteState = function(cp, cb0) {
+            if ((typeof cp === 'function') && (cb0 === undefined)) {
+                cb0 = cp;
+                cp = 'cp';
+            }
+            $._.$[cp].deleteState(uuid, cb0);
         };
 
-        that.getState = function(cb0) {
-            $._.$.cp.getState(uuid, cb0);
+        that.getState = function(cp, cb0) {
+            if ((typeof cp === 'function') && (cb0 === undefined)) {
+                cb0 = cp;
+                cp = 'cp';
+            }
+            $._.$[cp].getState(uuid, cb0);
         };
 
         cb(null, that);
--- test/test-redis.js
+++ test/test-redis.js
@@ -4,13 +4,10 @@ var hello = require('./hello/main.js');
 module.exports = {
     setUp: function (cb) {
         var self = this;
-        this.foo = 'bar';
         hello.load(null, null, 'hello1.json', null,
                    function(err, $) {
                        self.$ = $;
-                       console.log("Hello");
 //                       test.ifError(err);
-                       console.log('--> ');
 //                       test.equal(typeof($.topRedis), 'object',
 //                                  'Cannot create hello');
 
@@ -26,10 +23,8 @@ module.exports = {
         var state0;
         async.series([
                          function(cb) {
-                             console.log('-1> ');
                              state0 = self.$._.$.h2.readLocalState();
                              state0 = {counter: state0.counter};
-                             console.log('0> ' + JSON.stringify(state0));
                              self.$._.$.h2.grabLease(cb);
                          },
                          function(cb) {
@@ -90,7 +85,8 @@ module.exports = {
                          },
                          function(cb) {
                              var cb1 = function(err, data) {
-                                 test.equal(data, null, 'Deleted key has value ' +
+                                 test.equal(data, null,
+                                            'Deleted key has value ' +
                                             data);
                                  cb(err, data);
                              };
@@ -167,7 +163,7 @@ module.exports = {
     bulk: function(test) {
         var self = this;
         var all = ['h1','h2','h3','h4','h5','h6','h7','h8','h9'];
-        test.expect(19);
+        test.expect(28);
         async.series([
                          function(cb) {
                              async.map(all, function(x, cb0) {
@@ -198,11 +194,166 @@ module.exports = {
                                            };
                                            self.$._.$[x].getState(cb1);
                                        }, cb);
+                         },
+                         function(cb) {
+                             async.map(all, function(x, cb0) {
+                                           self.$._.$[x].deleteState(cb0);
+                                       }, cb);
+                         },
+                        function(cb) {
+                             async.map(all, function(x, cb0) {
+                                           var cb1 = function(err, data) {
+                                               test.equal(data, null,
+                                                          'Deleted key exists' +
+                                                          data);
+                                               cb0(err, data);
+                                           };
+                                           self.$._.$[x].getState(cb1);
+                                       }, cb);
+                         }
+                     ], function(err, data) {
+                         test.ifError(err);
+                         test.done();
+                     });
+    },
+    twoPlugs: function(test) {
+        var self = this;
+        test.expect(6);
+        async.series([
+                         function(cb) {
+                             self.$.topRedis.__ca_shutdown__(null, cb);
+                         },
+                         function(cb) {
+                             hello.load(null, null, 'hello2.json', null,
+                                        function(err, $) {
+                                            self.$ = $;
+                                            test.ifError(err);
+                                            test.equal(typeof($.topRedis),
+                                                       'object',
+                                                       'Cannot create hello');
+                                            cb(err, $);
+                                        });
+                         },
+                         function(cb) {
+                            self.$._.$.h2.grabLease('cp', cb);
+                         },
+                         function(cb) {
+                             self.$._.$.h2.updateState('cp', cb);
+                         },
+                         function(cb) {
+                             var cb1 = function(err, data) {
+                                 test.ok(err && (typeof err === 'object'),
+                                         "No error with update with bad lease");
+                                 cb(null);
+                             };
+                             self.$._.$.h2.updateState('cp2', cb1);
+                         },
+                         function(cb) {
+                             var cb1 = function(err, data) {
+                                 test.ok(err && (typeof err === 'object'),
+                                         "No error stealing lease");
+                                 test.equal(typeof err.remoteNode, 'string',
+                                            'No remote node');
+                                 cb(null);
+                             };
+                             self.$._.$.h2.grabLease('cp2', cb1);
+                         },
+                         function(cb) {
+                             setTimeout(function() { cb(null); }, 1500);
+                         },
+                         function(cb) {
+                             self.$._.$.h2.grabLease('cp2', cb);
+                         },
+                         function(cb) {
+                             self.$._.$.h2.updateState('cp2', cb);
+                         }
+                     ], function(err, data) {
+                         test.ifError(err);
+                         test.done();
+                     });
+    },
+    bulkTwoPlugs: function(test) {
+        var self = this;
+        var all = ['h2','h3'];
+        test.expect(10);
+        async.series([
+                         function(cb) {
+                             self.$.topRedis.__ca_shutdown__(null, cb);
+                         },
+                         function(cb) {
+                             hello.load(null, null, 'hello2.json', null,
+                                        function(err, $) {
+                                            self.$ = $;
+                                            test.ifError(err);
+                                            test.equal(typeof($.topRedis),
+                                                       'object',
+                                                       'Cannot create hello');
+                                            cb(err, $);
+                                        });
+                         },
+                         function(cb) {
+                            self.$._.$.h2.grabLease('cp', cb);
+                         },
+                         function(cb) {
+                            self.$._.$.h3.grabLease('cp2', cb);
+                         },
+                         function(cb) {
+                             async.map(all, function(x, cb0) {
+                                           if (x === 'h2') {
+                                               self.$._.$[x]
+                                                   .updateState('cp', cb0);
+                                           } else {
+                                               var cb1 = function(err) {
+                                                   var t = err &&
+                                                       (typeof err ==='object');
+                                                   test.ok(t, "No update err");
+                                                   cb0(null);
+                                               };
+                                              self.$._.$[x]
+                                                   .updateState('cp', cb1);
+                                           }
+                                       }, cb);
+                         },
+                         function(cb) {
+                             async.map(all, function(x, cb0) {
+                                           if (x === 'h2') {
+                                               self.$._.$[x]
+                                                   .updateState('cp', cb0);
+                                           } else {
+                                               self.$._.$[x]
+                                                   .updateState('cp2', cb0);
+                                           }
+                                       }, cb);
+                         },
+                         function(cb) {
+                            var cb0 = function(err, data) {
+                                 test.ifError(err);
+                                 var state = self.$._.$.h2.readLocalState();
+                                 test.equal(state.counter, 2,
+                                            'h2 counter not incremented');
+                                 var cpState = JSON.parse(data);
+                                 test.equal(state.counter, cpState.counter,
+                                            'State CP not incremented');
+                                 cb(err, data);
+                             };
+                             self.$._.$.h2.getState('cp', cb0);
+                         },
+                         function(cb) {
+                            var cb0 = function(err, data) {
+                                 test.ifError(err);
+                                 var state = self.$._.$.h3.readLocalState();
+                                 test.equal(state.counter, 1,
+                                            'h3 counter not incremented');
+                                 var cpState = JSON.parse(data);
+                                 test.equal(state.counter, cpState.counter,
+                                            'State CP not incremented');
+                                 cb(err, data);
+                             };
+                             self.$._.$.h3.getState('cp2', cb0);
                          }
                      ], function(err, data) {
                          test.ifError(err);
                          test.done();
                      });
     }
-
 };
